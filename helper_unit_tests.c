ce1f53a179aceaf2681c5fa4b0d4d5a8fd08f8e8
David Lee Ramirez <lee@leapwithalice.io>
2019-03-18 16:12:41 -0700

    complete repeated random testing added to stringToHex_test()

--- helper_unit_tests.c
+++ helper_unit_tests.c
@@ -14,6 +14,7 @@
 #define NUM_TESTS 2
 #define TEST_DIR_NAME "testdir"
 #define NUM_FILES_TEST_DIR 4
+#define NUM_TEST_REPEATS 1000
 
 
 int stringToHex_test()
@@ -21,60 +22,61 @@ int stringToHex_test()
     const char *hexPool = "0123456789abcdef";
     char testKey[65];
     const char* spacedTestKey; 
-    uint8_t *serializedTestKey = malloc(sizeof(uint8_t)*32);
-    FILE *testStringFilePointer = fopen("testStringFile", "a+");
-    FILE *testHexFilePointer = fopen("testHexFile", "a+");
     char readTestHexFile[65];
     char readTestStringFile[65];
 
-    //randomly generate test key
-    for (int i = 0; i < 64; i++)
+    //repeat this test NUM_TEST_REPEATS times
+    for (int m = 0; m < NUM_TEST_REPEATS; m++)
     {
-        testKey[i] = hexPool[rand() % 16];
+        uint8_t *serializedTestKey = malloc(sizeof(uint8_t)*32);
+        FILE *testStringFilePointer = fopen("testStringFile", "a+");
+        FILE *testHexFilePointer = fopen("testHexFile", "a+");
+
+        //randomly generate test key
+        for (int i = 0; i < 64; i++)
+        {
+            testKey[i] = hexPool[rand() % 16];
+        }
+        testKey[64] = "\0";
+
+        //add spaces to string to separate hex values
+        spacedTestKey = insertSpaces(testKey);
+
+        //convert test key (with spaces) into hex
+        serializedTestKey = stringToHex(spacedTestKey);
+
+        //print original testKey string into test file
+        for (int i = 0; i < 64; i++)
+        {
+            fprintf(testStringFilePointer, "%c", testKey[i]);
+        }
+        //print hex converted testKey uint8_t into a seperate test file (printed as hex)
+        for (int i = 0; i < 32; i++)
+        {
+            fprintf(testHexFilePointer, "%02x", serializedTestKey[i]);
+        }
+        rewind(testStringFilePointer);
+        rewind(testHexFilePointer);
+
+        //read both strings from test files into seperate strings
+        fgets(readTestStringFile, 65, (FILE*)testStringFilePointer);
+        fgets(readTestHexFile, 65, (FILE*)testHexFilePointer);
+
+        remove("testStringFile");
+        remove("testHexFile");
+        fclose(testHexFilePointer);
+        fclose(testStringFilePointer);
+
+        //compare both strings from test files, if same then test passes
+        if (strcmp(readTestStringFile, readTestHexFile) != 0)
+        {
+            printf("1) stringToHex_test FAILED\n");
+            return 1;
+        }
+        free(serializedTestKey);
     }
-    testKey[64] = "\0";
 
-    //add spaces to string to separate hex values
-    spacedTestKey = insertSpaces(testKey);
-
-    //convert test key (with spaces) into hex
-    serializedTestKey = stringToHex(spacedTestKey);
-
-    //print original testKey string into test file
-    for (int i = 0; i < 64; i++)
-    {
-        fprintf(testStringFilePointer, "%c", testKey[i]);
-    }
-    //print hex converted testKey uint8_t into a seperate test file (printed as hex)
-    for (int i = 0; i < 32; i++)
-    {
-        fprintf(testHexFilePointer, "%02x", serializedTestKey[i]);
-    }
-    rewind(testStringFilePointer);
-    rewind(testHexFilePointer);
-
-    //read both strings from test files into seperate strings
-    fgets(readTestStringFile, 65, (FILE*)testStringFilePointer);
-    fgets(readTestHexFile, 65, (FILE*)testHexFilePointer);
-
-    remove("testStringFile");
-    remove("testHexFile");
-    fclose(testHexFilePointer);
-    fclose(testStringFilePointer);
-
-    //compare both strings from test files, if same then test passes
-    if (strcmp(readTestStringFile, readTestHexFile) == 0)
-    {
-        printf("1) stringToHex_test passed\n");
-    }
-    else
-    {
-        printf("1) stringToHex_test FAILED\n");
-        return 1;
-    }
-
-    free(serializedTestKey);
-
+    printf("1) stringToHex_test passed\n");
     return 0;
 }
 
